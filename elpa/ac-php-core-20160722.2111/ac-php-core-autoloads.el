da0b6536f9be8bc23a29b096266397b448a5dfe6
Yunhao <yz2327@cornell.edu>
2017-09-15 23:00:54 -0400

    Update and support Golang

--- elpa/ac-php-core-20160722.2111/ac-php-core-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; ac-php-core-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("ac-php-comm-tags-data.el" "ac-php-core-pkg.el"
-;;;;;;  "ac-php-core.el") (22442 39487 419418 998000))
-
-;;;***
-
-(provide 'ac-php-core-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; ac-php-core-autoloads.el ends here
---
+++ elpa/ac-php-core-20170831.802/ac-php-core-autoloads.el
@@ -0,0 +1,27 @@
+;;; ac-php-core-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "ac-php-core" "ac-php-core.el" (22972 35756
+;;;;;;  430260 917000))
+;;; Generated autoloads from ac-php-core.el
+
+(autoload 'ac-php-core-eldoc-setup "ac-php-core" "\
+Set up eldoc function and enable eldoc-mode.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("ac-php-comm-tags-data.el" "ac-php-core-pkg.el")
+;;;;;;  (22972 35756 446754 893000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ac-php-core-autoloads.el ends here
